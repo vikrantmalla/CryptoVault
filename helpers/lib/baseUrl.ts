accf20306983698b5cc82ecde6bed4c2f91f2782
bikrant malla <bikrant.malla@truenary.com>
2023-04-17 22:47:48 +0545

    feat: setup a post api and mongodb

---
+++ helpers/lib/baseUrl.ts
@@ -0,0 +1,2 @@
+const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_APP_URL  : process.env.NEXT_PUBLIC_DEFAULT_APP_URL
+export default baseUrl
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,11 +1,59 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    // ...add more providers here
-  ],
-};
+import { compare } from 'bcryptjs'
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { connectToMongoDB } from "@/helpers/lib/dbConnect"
+import { IUser } from "@/types/data"
+import User from '@/model/user'
 
 
-export default NextAuth(authOptions)
+const options: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                await connectToMongoDB().catch(err => { throw new Error(err) })
+
+                const user = await User.findOne({
+                    email: credentials?.email
+                }).select("+password")
+
+                if (!user) {
+                    throw new Error("Invalid credentials")
+                }
+
+                const isPasswordCorrect = await compare(credentials!.password, user.password)
+
+                if (!isPasswordCorrect) {
+                    throw new Error("Invalid credentials")
+                }
+
+                return user
+            }
+        })
+    ],
+    pages: {
+        signIn: "/login"
+    },
+    session: {
+        strategy: "jwt"
+    },
+    callbacks: {
+        jwt: async ({ token, user }) => {
+            user && (token.user = user)
+            return token
+        },
+        session: async ({ session, token }) => {
+            const user = token.user as IUser
+            session.user = user
+
+            return session
+        }
+    }
+}
+
+export default NextAuth(options)

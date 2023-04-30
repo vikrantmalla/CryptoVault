738c8cd5f8ffc1a7611c00a5330ac2c2d76483db
bikrant malla <bikrant.malla@truenary.com>
2023-04-30 11:32:33 +0545

    chore: change code structure

--- pages/api/auth/signup.tsx
+++ pages/api/auth/signup.tsx
@@ -7,50 +7,49 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   connectToMongoDB().catch((err) => res.json(err));
 
   if (req.method === "POST") {
-    if (req.method === "POST") {
-      if (!req.body) return res.status(400).json({ error: "Data is missing" });
+    if (!req.body) return res.status(400).json({ error: "Data is missing" });
 
-      const { email, password } = req.body;
+    const { email, password } = req.body;
 
-      const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email });
 
-      if (userExists) {
-        return res.status(409).json({ error: "User Already exists" });
-      } else {
-        if (password.length < 6)
-          return res
-            .status(409)
-            .json({ error: "Password should be 6 characters long" });
-
-        try {
-          const user = await User.create({
-            email,
-            password,
-          });
-
-          const responseData = {
-            email: user.email,
-            _id: user._id,
-          };
-
-          return res.status(201).json({
-            success: true,
-            user: responseData,
-          });
-        } catch (error: unknown) {
-          if (error instanceof mongoose.Error.ValidationError) {
-            for (let field in error.errors) {
-              const msg = error.errors[field].message;
-              return res.status(409).json({ error: msg });
-            }
-          }
-          
-          return res.status(500).json({ error: "Something went wrong" });
-        }
-      }
+    if (userExists) {
+      return res.status(409).json({ error: "User Already exists" });
     } else {
-      res.status(405).json({ error: "Method Not Allowed" });
+      if (password.length < 4)
+        return res
+          .status(409)
+          .json({ error: "Password should be 4 characters long" });
+
+      try {
+        const user = await User.create({
+          email,
+          password,
+        });
+
+        const responseData = {
+          email: user.email,
+          _id: user._id,
+        };
+
+        return res.status(201).json({
+          success: true,
+          message: 'Signup Sucessfully',
+          user: responseData,
+        });
+      } catch (error: unknown) {
+        if (error instanceof mongoose.Error.ValidationError) {
+          for (let field in error.errors) {
+            const msg = error.errors[field].message;
+            return res.status(409).json({ error: msg });
+          }
+        }
+
+        return res.status(500).json({ error: "Something went wrong" });
+      }
     }
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed" });
   }
 };
 

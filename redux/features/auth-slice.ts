fe20558e2d96fa57f2d4b7fbfb5d4f1cb1869fe2
Vikrant Malla <vikrantmalla999@gmail.com>
2023-06-17 22:06:00 +0545

    refactor: add a usestate in reducer

--- redux/features/auth-slice.ts
+++ redux/features/auth-slice.ts
@@ -1,14 +1,20 @@
 import { AuthState, InitialState } from '@/types/data'
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-const initialState = {
-    value: {
-        showModal: false
-    } as AuthState
-} as InitialState
+const initialState: AuthState = {
+  showModal: false,
+};
 
 export const auth = createSlice({
   name: "auth",
   initialState,
-  reducers: {}
+  reducers: {
+    setShowModal: (state, action) => {
+      state.showModal = action.payload;
+    },
+  }
 })
+
+
+export const {setShowModal} = auth.actions;
+export default auth.reducer;
